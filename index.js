32a98e653ac1f3dcf661d2a5ef50678891e2a303
indumatic <electrico@indumatic.net>
2025-01-28 11:10:12 +0100

    create person model

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 //custom token for body
 morgan.token('body', (req, res) => JSON.stringify(req.body))
